7c415c047503096b3d54abfa50b1d4f2ffabc9c2
unknown <daniela.nicolia.patrut@gmail.com>
2022-01-06 21:07:35 +0200

    created firebase connection and welcome page UI

---
+++ lib/constants/constants.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+Color defaultColor = const Color.fromRGBO(126, 49, 186, 1.0);
+Color? googleTextColor = Colors.grey[600];
+
+
+//Styles
+TextStyle headerTextStyle = TextStyle(
+    color: defaultColor,
+    fontSize: 24,
+    fontWeight: FontWeight.bold
+);
+
+
+TextStyle paragraphTextStyle = TextStyle(
+    color: defaultColor,
+    fontSize: 14
+);
