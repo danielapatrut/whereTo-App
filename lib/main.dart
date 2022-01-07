2f7f23635ff5f3a687cfb857b29d57762ac6c74d
unknown <daniela.nicolia.patrut@gmail.com>
2022-01-07 12:05:52 +0200

    delete unnecessary files

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:where_to_app/controllerLogin.dart';
-import 'package:where_to_app/screens/loginScreen.dart';
 import 'package:where_to_app/screens/welcomeScreen.dart';
-/*
-void main() {
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => ControllerLogin() )
-      ],
-      child: MaterialApp(
-        title: 'WhereTo-App',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: LoginScreen(),
-      )
-      );
-  }
-}
-*/
 void main() {
   runApp(const WhereToApp());
 }
